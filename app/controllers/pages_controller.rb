a1f55845cd6d0b636c25b8529a93dd55859f8d92
Carla Valdivia <valdivia.carla@gmail.com>
2023-08-28 10:24:33 -0300

    pages controller - index show new create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  #Humberto
+  def index
+  end
+
+  #Pedro
+  def show
+  end
+
+  #Matt
+  def new
+  end
+
+  def create
+  end
 end
